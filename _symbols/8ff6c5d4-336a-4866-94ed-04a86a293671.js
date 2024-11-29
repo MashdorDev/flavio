1db9c06fb5219609f0ab1993d097535044dcb648
Dor <56084381+MashdorDev@users.noreply.github.com>
2024-11-28 20:41:24 -0600

    Deploy site

--- _symbols/8ff6c5d4-336a-4866-94ed-04a86a293671.js
+++ _symbols/8ff6c5d4-336a-4866-94ed-04a86a293671.js
@@ -4042,7 +4042,7 @@ function instance($$self, $$props, $$invalidate) {
 
 	async function submit_form(e) {
 		const form_data = get_form_data(e.target);
-		const { data } = await axios.post("https://formsubmit.co/dorzairi@ymail.com", form_data).catch(e => ({ data: null }));
+		const { data } = await axios.post("https://formsubmit.co/contadores3934wtc@gmail.com", form_data).catch(e => ({ data: null }));
 
 		if (data) {
 			$$invalidate(4, submitted = true);
